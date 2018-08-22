f41bad9710cc2df804fd3dbbb6ff076296d6948d
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-08-22 18:07:10 +0200

    Remove not long used template

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,12 +2,10 @@ import CMS from 'netlify-cms'
 
 import YoutubeWidget from './widgets/Youtube'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerEditorComponent(YoutubeWidget)
 
-CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
