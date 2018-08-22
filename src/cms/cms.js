7b59854470b63c7a23b7b1785a6700a06505b8fb
Andrea Mangano <andrea.mangano@ibuildings.it>
2018-08-22 17:52:56 +0200

    Fix

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,12 @@
 import CMS from 'netlify-cms'
 
+import YoutubeWidget from './widgets/Youtube'
+
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+
+CMS.registerEditorComponent(YoutubeWidget)
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
